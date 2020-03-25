effbdaf6f2fda99b0745ff39479c02c557c0abee
Don Stolz <donaldtstolz@gmail.com>
2020-03-25 11:38:31 -0400

    Add fetchPosts

--- src/Home.tsx
+++ src/Home.tsx
@@ -26,6 +26,7 @@ const Home: React.FunctionComponent = () => {
   const classes = useStyles();
   const [loading, setLoading] = React.useState<boolean>(true);
   const [posts, setPosts] = React.useState<any[]>([]);
+
   const initData = async () => {
     const providerBox = await Box.create((window as any).ethereum);
     const address = "0x47b0fde4622577cb10ed2c79108d79cbb582ee5c";
@@ -38,9 +39,33 @@ const Home: React.FunctionComponent = () => {
     console.log("finish space sync");
     const spaceMetaData = await space.public.get("blog-meta");
     console.log(spaceMetaData);
+    // const newPosts =
+    await fetchPosts(spaceMetaData);
     // setPosts(featuredPosts);
     setLoading(false);
   };
+
+  const fetchPosts = async (metaData: {
+    numPosts: number;
+    posts: Array<string>;
+  }) => {
+    console.log("in fetchPosts");
+
+    const newPosts = [];
+    for (let index = 0; index < metaData.posts.length; index++) {
+      console.log("in loop");
+      const ipfsHash = metaData.posts[index];
+      console.log(ipfsHash);
+      const newData = await fetch(
+        `https://gateway.pinata.cloud/ipfs/${ipfsHash}/meta-data.json`
+      ).then(res => res.json());
+      newData["id"] = ipfsHash;
+      console.log(newData);
+      newPosts.push(newData);
+    }
+    console.log(newPosts);
+  };
+
   React.useEffect(() => {
     // Posts from 3Box
     void initData();
