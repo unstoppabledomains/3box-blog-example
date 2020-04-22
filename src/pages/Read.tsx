c3ea997fdf707ba4b809a0b0648ba8975c27ee9f
Don Stolz <donaldtstolz@gmail.com>
2020-04-22 13:00:18 -0400

    Update Read & Write styles

--- src/pages/Read.tsx
+++ src/pages/Read.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
-import { BlogPost } from "types/blog";
+import { BlogPost, ThreadObject } from "types/blog";
 import Markdown from "react-showdown";
 import { showdownOptions } from "config/showdown";
 import { useHistory, useParams } from "react-router-dom";
@@ -13,14 +13,49 @@ import { login } from "services/userActions";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {},
+    root: {
+      display: "flex",
+      flexDirection: "column",
+      maxWidth: 800,
+      marginRight: "auto",
+      marginLeft: "auto",
+      marginTop: theme.spacing(2),
+      marginBottom: theme.spacing(4),
+      paddingRight: theme.spacing(2),
+      paddingLeft: theme.spacing(2),
+    },
+    titleContainer: {
+      display: "flex",
+      flexDirection: "column",
+      marginBottom: theme.spacing(2),
+    },
+    title: {},
+    author: {},
+    description: {},
+    bodyContainer: {
+      display: "flex",
+      marginBottom: theme.spacing(4),
+    },
+    commentContainer: {
+      marginRight: "auto",
+      marginLeft: "auto",
+      maxWidth: 616,
+      overflowX: "hidden",
+      paddingLeft: theme.spacing(1),
+      paddingRight: theme.spacing(1),
+    },
+    loadingContainer: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      minHeight: "60vh",
+    },
   })
 );
 
 // TODO
 /**
  * [] Auth w/ 3Box Profiles
- * [] Comments w/ 3Box Plugin
  */
 
 const ReadPost: React.FunctionComponent = () => {
@@ -58,27 +93,47 @@ const ReadPost: React.FunctionComponent = () => {
   return (
     <div className={classes.root}>
       {loading ? (
-        <CircularProgress />
+        <div className={classes.loadingContainer}>
+          <CircularProgress />
+        </div>
       ) : (
         <>
-          <Typography variant="h2">{post.title}</Typography>
-          <Markdown
-            dangerouslySetInnerHTML
-            markdown={post.body}
-            options={showdownOptions}
-          />
-          <Comments
-            // required
-            spaceName={spaceName}
-            threadName={`comments-${postId}`}
-            adminEthAddr={process.env.REACT_APP_ADMIN_WALLET as string}
-            // Required props for context A) & B)
-            // FIXME: issues with null instance right after login; Can't update with auth box or authenticate box issues
-            box={userAddress ? box : null}
-            currentUserAddr={userAddress}
-            // Required prop for context B)
-            loginFunction={handleLogin}
-          />
+          <div className={classes.titleContainer}>
+            <Typography className={classes.title} variant="h2" gutterBottom>
+              {post.title}
+            </Typography>
+            <Typography
+              className={classes.author}
+              variant="subtitle2"
+              gutterBottom
+            >
+              By {(post.threadData as ThreadObject).author}
+            </Typography>
+            <Typography className={classes.description} variant="subtitle1">
+              {post.description}
+            </Typography>
+          </div>
+          <div className={classes.bodyContainer}>
+            <Markdown
+              dangerouslySetInnerHTML
+              markdown={post.body}
+              options={showdownOptions}
+            />
+          </div>
+          <div className={classes.commentContainer}>
+            <Comments
+              // required
+              spaceName={spaceName}
+              threadName={`comments-${postId}`}
+              adminEthAddr={process.env.REACT_APP_ADMIN_WALLET as string}
+              // Required props for context A) & B)
+              // FIXME: issues with null instance right after login; Can't update with auth box or authenticate box issues
+              box={userAddress ? box : null}
+              currentUserAddr={userAddress}
+              // Required prop for context B)
+              loginFunction={handleLogin}
+            />
+          </div>
         </>
       )}
     </div>
--- src/pages/Write.tsx
+++ src/pages/Write.tsx
@@ -1,79 +1,143 @@
 import React from "react";
-import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
-import { useHistory, Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { BlogPost, ThreadObject } from "types/blog";
 import Editor from "components/Editor";
+import { addPost } from "services/blogActions";
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 import CircularProgress from "@material-ui/core/CircularProgress";
-import { addPost, getPosts } from "services/blogActions";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {},
+    root: {
+      display: "flex",
+      flexDirection: "column",
+      maxWidth: 1200,
+      marginRight: "auto",
+      marginLeft: "auto",
+      marginTop: theme.spacing(2),
+      marginBottom: theme.spacing(4),
+      paddingRight: theme.spacing(2),
+      paddingLeft: theme.spacing(2),
+    },
+    title: {
+      fontSize: 36,
+    },
+    buttonRow: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "space-between",
+      marginTop: theme.spacing(3),
+    },
+    textField: {
+      marginBottom: theme.spacing(2),
+    },
+    button: {
+      height: 56,
+      fontWeight: "bold",
+      fontSize: 18,
+      color: "#fff",
+    },
+    destroyBtn: {
+      backgroundColor: "#d60000",
+      "&:hover": {
+        backgroundColor: "#9b0000",
+      },
+      marginRight: theme.spacing(8),
+    },
+    saveBtn: {
+      backgroundColor: "#28a745",
+      "&:hover": {
+        backgroundColor: "#007717",
+      },
+    },
   })
 );
-
-// TODO
-/**
- * [] Check Auth (context)
- */
 
 const WritePost: React.FunctionComponent = () => {
   const classes = useStyles();
   const history = useHistory();
   const [loading, setLoading] = React.useState<boolean>(false);
   const [post, setPost] = React.useState<BlogPost>({
-    title: "TITLE",
-    description: "lorem ipsum stuff",
-    tags: ["test", "new", "don"],
+    tags: [""],
   } as BlogPost);
 
-  const setPostBody = (body: string) => setPost({ ...post, body });
+  const handleBodyChange = (body: string) => setPost({ ...post, body });
   //   const setPostAttributes = (body: string) => setPost({ ...post, body });
+  const handleChange = ({
+    target: { value, id },
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    setPost({ ...post, [id]: value });
+  };
 
   const onSave = async () => {
     setLoading(true);
     try {
       const postId = await addPost(post);
       setPost({ ...post, threadData: { postId } as ThreadObject });
-      setLoading(false);
-      //   history.push(`/posts/${id}`);
+      history.push(`/posts/${postId}`);
     } catch (error) {
       // TODO handle error
+      console.error(error);
     }
   };
-  //   React.useEffect(() => {}, []);
 
   const onDestroy = () => {
-    console.log("Destroy");
+    setPost({} as BlogPost);
   };
 
   return (
     <div className={classes.root}>
+      <Typography variant="h1" className={classes.title} gutterBottom>
+        Draft
+      </Typography>
       {loading ? (
         <CircularProgress />
       ) : (
         <>
-          <Editor value={post.body} onChange={setPostBody} />
-          <div>
-            <nav>
-              <Button type="submit" disabled={loading} onClick={onSave}>
-                Save
-              </Button>
-            </nav>
-            <nav>
-              <Button type="button" disabled={loading} onClick={onDestroy}>
-                Destroy
-              </Button>
-            </nav>
-          </div>
-          <div>
-            {post.threadData && post.threadData.postId && (
-              <>
-                ➡
-                <Link to={`/post/${post.threadData.postId}`}>link to post</Link>
-              </>
-            )}
+          <TextField
+            variant="outlined"
+            id="title"
+            value={post.title}
+            onChange={handleChange}
+            label="Title"
+            className={classes.textField}
+          />
+          <TextField
+            variant="outlined"
+            id="description"
+            value={post.description}
+            onChange={handleChange}
+            label="Description"
+            multiline
+            rows={2}
+            rowsMax={3}
+            className={classes.textField}
+          />
+          <Editor value={post.body} onChange={handleBodyChange} />
+          <div className={classes.buttonRow}>
+            <Button
+              className={`${classes.button} ${classes.destroyBtn}`}
+              fullWidth
+              type="button"
+              disabled={loading}
+              onClick={onDestroy}
+              variant="contained"
+            >
+              Destroy
+            </Button>
+            <Button
+              className={`${classes.button} ${classes.saveBtn}`}
+              fullWidth
+              type="submit"
+              disabled={loading}
+              onClick={onSave}
+              variant="contained"
+            >
+              Save
+            </Button>
           </div>
         </>
       )}
